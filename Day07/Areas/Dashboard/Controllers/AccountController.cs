c5d5008c81569f47798d64887d5cec63e6847017
ilker turan <ilkerturan@outlook.com>
2023-01-25 15:59:09 +0300

    16:00

--- Day07/Areas/Dashboard/Controllers/AccountController.cs
+++ Day07/Areas/Dashboard/Controllers/AccountController.cs
@@ -4,6 +4,9 @@ using Day07.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Configuration;
+using System.Net.WebSockets;
 
 namespace Day07.Areas.Dashboard.Controllers
 {
@@ -12,17 +15,87 @@ namespace Day07.Areas.Dashboard.Controllers
     public class AccountController : Controller
     {
         private UserManager<CustomUser> _userManager;
+        private RoleManager<CustomRole> _roleManager;
         private IMapper _mapper;
 
-        public AccountController(UserManager<CustomUser> userManager, IMapper mapper)
+        public AccountController(UserManager<CustomUser> userManager, IMapper mapper, RoleManager<CustomRole> roleManager)
         {
             _userManager = userManager;
             _mapper = mapper;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
         {
             return View(AccountService.Get().GetAccounts(_userManager, _mapper));
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id.Equals(id));
+            var userRoles = await _userManager.GetRolesAsync(user);
+            ViewBag.Roles = _roleManager.Roles.Select(x => new SelectListItem()
+            {
+                Value = x.Name,
+                Text = x.Name,
+                Selected = userRoles.Contains(x.Name)
+            });
+            return View(_userManager.Users.FirstOrDefault(x => x.Id.Equals(id)));
+        }
+
+        public bool Toggle(string p, string id)
+        {
+            bool result = false;
+            var selectedUser = _userManager.Users.FirstOrDefault(x => x.Id.Equals(id));
+            switch (p)
+            {
+                case "active":
+                    selectedUser.Active = !selectedUser.Active;
+                    result = selectedUser.Active;
+                    break;
+                case "deleted":
+                    selectedUser.Deleted = !selectedUser.Deleted;
+                    result = selectedUser.Deleted;
+                    break;
+                case "email_confirmed":
+                    selectedUser.EmailConfirmed = !selectedUser.EmailConfirmed;
+                    result = selectedUser.EmailConfirmed;
+                    break;
+                case "phone_number_confirmed":
+                    selectedUser.PhoneNumberConfirmed = !selectedUser.PhoneNumberConfirmed;
+                    result = selectedUser.PhoneNumberConfirmed;
+                    break;
+                case "two_factor_enabled":
+                    selectedUser.TwoFactorEnabled = !selectedUser.TwoFactorEnabled;
+                    result = selectedUser.TwoFactorEnabled;
+                    break;
+                case "lockout_enabled":
+                    selectedUser.LockoutEnabled = !selectedUser.LockoutEnabled;
+                    result = selectedUser.LockoutEnabled;
+                    break;
+            }
+            _userManager.UpdateAsync(selectedUser);
+
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(string id, string role)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id.Equals(id));
+            if (user != null)
+            {
+                if (await _userManager.IsInRoleAsync(user, role))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, role);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, role);
+                }
+            }
+
+            return RedirectToAction(nameof(Details), new { id });
         }
     }
 }
---
+++ Day07/Areas/Dashboard/Controllers/RoleController.cs
@@ -0,0 +1,105 @@
+﻿using Day07.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day07.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class RoleController : Controller
+    {
+        private UserManager<CustomUser> _userManager;
+        private RoleManager<CustomRole> _roleManager;
+        private IRoleStore<CustomRole> _roleStore;
+
+        public RoleController(UserManager<CustomUser> userManager, RoleManager<CustomRole> roleManager, IRoleStore<CustomRole> roleStore)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;            _roleStore = roleStore;
+
+        }
+
+        // GET: RoleController
+        public ActionResult Index()
+        {
+            return View(_roleManager.Roles);
+        }
+
+        // GET: RoleController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RoleController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RoleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(string Name)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                CustomRole role = Activator.CreateInstance<CustomRole>();
+                await _roleStore.SetRoleNameAsync(role, Name, CancellationToken.None);
+                try
+                {
+                    await _roleManager.CreateAsync(role);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    return View(role);
+                }
+            }
+            return View();
+        }
+
+        // GET: RoleController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RoleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RoleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RoleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
